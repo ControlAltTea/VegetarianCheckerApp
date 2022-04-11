07bf4bfed04af235f28d3c79d8513bcb6bf2032c
Tea M <teadevcode@gmail.com>
2022-04-11 14:59:59 -0700

    Created table and tested barcode
    Barcode returns the object information if the product's barcode corresponds with a valid product

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,20 @@
+console.log("connected to main.js")
 
+function getFetch() {
+    let inputVal = document.getElementById('barcode').value;
+    const url = `https://world.openfoodfacts.org/api/v0/product/${inputVal}.json`;
 
-function getFetch(userInput) {
-    const choice = userInput;
-    fetch(`https://world.openfoodfacts.org/api/v0/product/${choice}.json`)
+    fetch(url)
         .then(res => res.json())
         .then(data => {
             console.log(data)
+            // "status" has either a value of 1 if the product barcode is valid
+            // or a value of 0 if the product barcode is invalid
+            if (data.status === 1) {
+                // do this
+            }
         })
         .catch(err => {
             console.log(`error ${err}`)
-        })
-
+        });
 }
