2061addd58e96364df8a0034f2e245c0d7082796
Tea M <teadevcode@gmail.com>
2022-04-13 17:43:57 -0700

    It is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,9 @@ console.log("connected to main.js")
 
 function getFetch() {
     let inputVal = document.getElementById('barcode').value;
+    // chicken tortilla soup = 011110038364
+    // beef and vegetable soup = 041196910759
 
-    
     const url = `https://world.openfoodfacts.org/api/v0/product/${inputVal}.json`;
 
     // Checks that the barcode is a valid length
@@ -23,6 +24,7 @@ function getFetch() {
                 const item = new ProductInfo(data.product);
                 item.testCall();
                 item.showInfo();
+                item.listIngredients();
             }
             else {
                 // If the barcode is invalid, the user will receive an alert to check it or use a different code
@@ -51,7 +53,60 @@ class ProductInfo {
     }
 
     showInfo() {
+        // pulling from the HTML, we can grab the element with an ID of "product-img" correlated to the product
         document.getElementById('product-img').src = this.image;
+        // pulling from the HTML, we can grab the element with an ID of "product-name" correlated to the product
         document.getElementById('product-name').innerText = this.name;
+    }
+
+    listIngredients() {
+
+        // accessing the table element in the HTML
+        let tableRef = document.getElementById('ingredient-table');
+
+        // Each time the getFetch() is called, the rows will be cleared out
+        // following the header, which is at the index 0 in the rows array
+        // prevents the table from retaining information from previous entered products
+        for (let i = 1; i < tableRef.rows.length; i++){
+            tableRef.deleteRow(i);
+        }
+
+        // loops through the properties in the ingredients object
+        for (let key in this.ingredients) {
+            // adds a row to the table ref
+            // we pass -1 to add a row to the end of the array of rows
+            let newRow = tableRef.insertRow(-1);
+            // one cell for the ingredients
+            // we place it at 0 to keep it at the start of the array of cells
+            let newICell = newRow.insertCell(0);
+            // one cell for the vegetarian category
+            // we place it at 1 to make it the second cell in the array of cells
+            let newVCell = newRow.insertCell(1);
+
+            // passing in text into the ICell
+            // the ingredients object has a key called "text", which is why we call for 'text'
+            let newIText = document.createTextNode(this.ingredients[key].text);
+            // same process, but with the vegetarian property
+            // we initialize a vegStatus in case the vegetarian property is "undefined"
+            // later, there will be a conditional to check for this
+            // We use a ternerary operator to check if the vegetarian status is undefined or null, and if it is, we instead return 'unknown', else we'll continue with the pre-determined response from the object's vegetarian property 
+            let vegStatus = this.ingredients[key].vegetarian == null ? 'unknown' : this.ingredients[key].vegetarian;
+            let newVText = document.createTextNode(vegStatus);
+            
+            // attachs text to the cell
+            newICell.appendChild(newIText);
+            // attachs text to the cell
+            newVCell.appendChild(newVText);
+
+            // *** CHANGES THE CELL COLOR ***
+            if (vegStatus === 'no') {
+                // turns item red
+                newVCell.classList.add('veg-item-no');
+            }
+            else if (vegStatus === 'unknown' || vegStatus === 'maybe') {
+                // turns item yellow
+                newVCell.classList.add('veg-item-maybe');
+            }
+        }
     }
 }
